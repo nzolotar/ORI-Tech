1058b6a861a7f8ce7c5cfca012f991aca6f4a0e1
Nat <nzolotar@users.noreply.github.com>
2024-11-05 19:36:03 -0500

    4 methods

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,24 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ConsoleApp2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("1:" + RemoveDuplicates("IXYZZI")); // IXYZ
             Console.WriteLine("2:" + MostEfficient("IXYZZI")); // IXYZ
+            Console.WriteLine("3:" + RemoveDuplicatesLinq("IXYZZI")); // IXYZ
+            Console.WriteLine("4:" + RemoveDuplicatesWithCount("IXYZZI")); // IXYZ
             Console.Read();
         }
 
+        //Using HashSet (maintains order of first occurrence)
         private static string MostEfficient(string word) {
             HashSet<char> uniqueChars = new HashSet<char>(word);
             return new string(uniqueChars.ToArray());
         }
+        //Using LINQ (shorter but might be less performant)
+        private static string RemoveDuplicatesLinq(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return new string(input.Distinct().ToArray());
+        }
+        // Using Dictionary
+        public static string RemoveDuplicatesWithCount(string input)
+        {
+            var charCount = new Dictionary<char, int>();
+            if (string.IsNullOrEmpty(input)) return input;
 
+            var result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount.Add(c, 1);
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
         private static string RemoveDuplicates(string word)
         {
             //verify if string is not blank 
