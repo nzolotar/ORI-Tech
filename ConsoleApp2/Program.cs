39e52aba86de309d2ac945a697f0d3f4233b0f98
Nat <nzolotar@users.noreply.github.com>
2024-11-05 19:03:04 -0500

    second problem

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(RemoveDuplicates("IXYZZI")); // IXYZ
+        }
+
+        private static string RemoveDuplicates(string word)
+        {
+            //verify if string is not blank 
+            if (string.IsNullOrWhiteSpace(word))
+                throw new Exception("No word work with");
+
+            List<char> processedCharacters = new List<char>();
+            List<char> result = new List<char>();
+
+            foreach (char character in word)
+            {
+                // find if it is in processedCharacters
+                var isItProcessed = processedCharacters.Contains(character);
+
+                if (!isItProcessed)
+                {
+                    result.Add(character);
+                    processedCharacters.Add(character);
+                }
+
+                //if processed and
+                if (isItProcessed)
+                {                    
+                    processedCharacters.Add(character);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
